4f0868ac736986f64758ef0a0c67d08f206ade0d
Seolchaehwan <seolchaehwan@naver.com>
2024-01-30 22:37:04 +0900

    다크모드 버튼 추가 & 네비바 완성

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Main from "./pages/main";
 import Projects from "./pages/projects";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
+import {createContext, useState} from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+    const [theme, setTheme] = useState("dark");
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
   return (
     <div className="App">
-      <BrowserRouter>
-          <MyNavbar />
-          <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/projects" element={<Projects />} />
-          </Routes>
-      </BrowserRouter>
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+          <BrowserRouter>
+              <MyNavbar current={theme} toggle={toggleTheme}/>
+              <Routes>
+                  <Route path="/" element={<Main />} />
+                  <Route path="/projects" element={<Projects />} />
+              </Routes>
+          </BrowserRouter>
+        </ThemeContext.Provider>
     </div>
   );
 }
--- src/etc/navbar.js
+++ src/etc/navbar.js
@@ -1,13 +1,27 @@
 import React from "react";
-import {Container} from "react-bootstrap";
-import {Navbar} from "react-bootstrap";
+import {Container, Navbar, Nav} from "react-bootstrap";
 import zerotoing from "../assets/zerotoing.png";
 import "./Navbar.css";
+import ReactSwitch from "react-switch";
+import {MdDarkMode, MdLightMode} from "react-icons/md";
+import {LinkContainer} from "react-router-bootstrap";
+
+const get_dark_icon = () => {
+    return (
+        <MdDarkMode className={"icons"}/>
+    )
+}
+
+const get_light_icon = () => {
+    return (
+        <MdLightMode className={"icons"}/>
+    )
+}
 
 const MyNavbar = (props) => {
     return (
       <div>
-        <Navbar className="bg-body-tertiary">
+        <Navbar className="bg-body-tertiary" data-bs-theme={props.current}>
             <Container>
                 <Navbar.Brand href="/" className="title">
                     <img
@@ -19,6 +33,18 @@ const MyNavbar = (props) => {
                     />{' '}
                     <b>Seolmango</b>
                 </Navbar.Brand>
+
+                <Navbar.Collapse>
+                    <Nav className="me-auto navbar-items">
+                        <LinkContainer to="/">
+                            <Nav.Link className="navbar-item">Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/projects">
+                            <Nav.Link className="navbar-item">Projects</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                    <ReactSwitch className={"switch-button"} onChange={props.toggle} checked={props.current === "dark"} checkedIcon={get_dark_icon()} uncheckedIcon={get_light_icon()} onColor={"#888"}/>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
       </div>
