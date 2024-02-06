06a064ec3f0c5fbed04caf6b881aa0cfdda263f0
Seolchaehwan <seolchaehwan@naver.com>
2024-02-06 23:29:14 +0900

    타이핑 패키지로 변경

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,43 +5,7 @@ import profile from "../assets/myprofile.jpeg";
 import {MdEmail} from "react-icons/md";
 import {FaInstagram, FaGithub} from "react-icons/fa";
 import {TbBrandYoutube} from "react-icons/tb";
-
-const typing_text_list = [
-    "I'm Seolmango!",
-    "I make what I want!",
-    "I'm a member of 0-inf!"
-];
-let typing_text = typing_text_list[0];
-let typing_text_list_index = 0;
-let typing_text_index = 1;
-let waitcount = 0;
-
-function typing() {
-    if(waitcount > 0){
-        waitcount--;
-        return;
-    }
-    if (typing_text_index < typing_text.length) {
-        let text = typing_text.charAt(typing_text_index);
-        const area = document.getElementById("typing");
-        area.innerHTML += text;
-        typing_text_index++;
-        if(typing_text_index === typing_text.length){
-            waitcount = 10;
-        }
-    }else{
-        typing_text_list_index++;
-        if(typing_text_list_index === typing_text_list.length){
-            typing_text_list_index = 0;
-        }
-        typing_text = typing_text_list[typing_text_list_index];
-        typing_text_index = 1;
-        const area = document.getElementById("typing");
-        area.innerHTML = typing_text.charAt(0);
-    }
-}
-
-const typingInterval = setInterval(typing, 200);
+import Typical from "react-typical";
 
 const Main = (props) => {
     return (
@@ -50,8 +14,11 @@ const Main = (props) => {
               <Col sm={8}>
                   <div className={"hello-area"}>
                       <h1 className={"title"}>Hello!</h1>
-                      <h1 className={"title inline"} id={"typing"}>I</h1>
-                      <h1 className={"title inline blink"}>|</h1>
+                      <Typical className={"title"}
+                          steps={['I\'m Seolmango!', 1500, 'I make what I want!', 1500, 'I\'m a member of 0-inf!', 1500]}
+                          loop={Infinity}
+                          wrapper="h1"
+                        />
                   </div>
               </Col>
               <Col sm={4}>
