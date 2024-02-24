511432699d98f21da2be6d68a57e3f5a91140539
smiilliin <smiilliindeveloper@gmail.com>
2024-02-24 12:14:42 +0900

    Change HashRouter and add eslint, some classes, keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import MyNavbar from "./etc/navbar";
 import Main from "./pages/main";
 import Projects from "./pages/projects";
 import Detail from "./pages/detail";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.css";
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <MyNavbar/>
-            <Routes>
-                <Route path="/" element={<Main/>} />
-                <Route path="/projects/detail/:name" element={<Detail/>} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/projects/:page" element={<Projects />} />
-            </Routes>
-        </BrowserRouter>
+      <HashRouter>
+        <MyNavbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/projects/detail/:name" element={<Detail />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:page" element={<Projects />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
